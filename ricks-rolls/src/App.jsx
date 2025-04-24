64c0a4c1f0ff59b70dacd3460a0e71a7f1cc4df3
CadenceReynolds <reynocal001@warren.k12.in.us>
2025-04-24 13:20:46 -0400

    Theme state between tabs

--- ricks-rolls/src/App.jsx
+++ ricks-rolls/src/App.jsx
@@ -1,14 +1,11 @@
-import ThemeProvider from "./ThemeProvider.jsx";
-import ThemeComponent from "./NavigationBar.jsx";
+import React from 'react';
 import Navbar from "./NavigationBar.jsx";
 
 export default function App() {
 
     return (
         <>
-            <ThemeProvider>
                 <Navbar></Navbar>
-            </ThemeProvider>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias commodi consequuntur dolorem doloribus eaque exercitationem iure nesciunt porro possimus. Accusamus accusantium consequuntur ea exercitationem facilis iure laboriosam obcaecati odit pariatur.</p>
 
         </>
--- ricks-rolls/src/ThemeContext.jsx
+++ ricks-rolls/src/ThemeContext.jsx
@@ -1,3 +1,29 @@
-import {createContext} from "react";
+import React, { createContext, useEffect, useState } from 'react';
 
-export const ThemeContext = createContext('light');
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState(() => localStorage.getItem('theme') || 'light');
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    // Sync across tabs
+    useEffect(() => {
+        const handleStorage = (e) => {
+            if (e.key === 'theme') {
+                setTheme(e.newValue);
+            }
+        };
+        window.addEventListener('storage', handleStorage);
+        return () => window.removeEventListener('storage', handleStorage);
+    }, []);
+
+    return (
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
